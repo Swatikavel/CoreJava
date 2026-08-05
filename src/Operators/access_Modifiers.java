a7bb5a567dd2cfa90dccb64f0864299bf4a75b2e
Swati <swati.h.k2103>
2026-08-05 17:01:24 +0530

    commit

---
+++ src/Operators/access_Modifiers.java
@@ -0,0 +1,15 @@
+package Operators;
+
+
+public class access_Modifiers {
+    public static void main (String[]args){
+
+    }
+    public class Vehicle {
+        // Accessible anywhere
+        public String brand = "Ford";
+
+        protected int speed = 70;
+        int year = 2026;
+    }
+    }
